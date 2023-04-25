cefe67ddda5659255740caad7e74bec247226301
TinyMurky <murky0830@gmail.com>
2023-04-25 19:39:14 +0800

    基本功能完成
    完成search與點入card內換頁

--- app.js
+++ app.js
@@ -6,4 +6,67 @@ const app = express()
 const exphbs = require("express-handlebars")
 
 //setup json
-const restau
+const restaurant = require("./restaurant.json")
+
+//setip port and hostname
+const port = 3000
+const hostname = "localhost"
+
+const setting = {
+  index:{
+    title:"我的餐廳",
+    stylesheet:"index.css",
+    restaurantList: restaurant.results,
+  },
+  show:{
+    title: null,
+    stylesheet:"show.css",
+    restaurantList: restaurant.results,
+    targetRest: null,
+  },
+}
+
+app.engine("handlebars", exphbs.engine({defaultLayout:"main"}))
+app.set("view engine", "handlebars")
+
+app.use(express.static("public"))
+
+//index render
+app.get("/", (req, res)=>{
+  res.status(200).render("index", setting.index)
+})
+
+//restaurant個別網頁介紹
+app.get("/restaurants/:id", (req, res)=>{
+  const id = req.params.id
+  const targetRest = restaurant.results.find(element=>element.id.toString() === id)
+  setting.show.targetRest = targetRest
+  setting.show.title = targetRest.name
+  if (targetRest){
+    res.status(200).render("show", setting.show)
+  } else {
+    res.status(404).send("<h1>404 Restaurant Not Found</h1>")
+  }
+})
+
+//搜尋功能
+app.get("/search", (req, res)=>{
+  const keyword = req.query.keyword
+  const keywordLower = keyword.toLowerCase().trim()
+  console.log(keywordLower)
+  const searchResults = restaurant.results.filter(element=>{
+    return element.name.toLowerCase().includes(keywordLower) ||
+    element.name_en.toLowerCase().includes(keywordLower)
+  })
+
+  //複製一個自己的setting
+  //但是不能複製nest的object要小心
+  const searchSetting = {...setting.index}
+  searchSetting.restaurantList = searchResults
+  searchSetting.keyword = keyword
+  res.status(200).render("index", searchSetting)
+})
+
+app.listen(port, hostname, ()=>{
+  console.log(`Server http://${hostname}:${port} started.`)
+})
