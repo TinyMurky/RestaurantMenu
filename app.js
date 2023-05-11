146cd685e8374e89a47e1b750e3dcc46cf9382fc
TinyMurky <murky0830@gmail.com>
2023-05-11 20:47:35 +0800

    Change gitignore and port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const Restaurant = require("./models/restaurant")
 //const restaurant = require("./restaurant.json")
 
 //setip port and hostname
-const port = process.env.PORT || 3000
+const port = Number(process.env.PORT) || 3000
 const hostname = "localhost"
 
 // handlebar setting
