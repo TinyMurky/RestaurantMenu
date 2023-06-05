b62cc33b15d3d35e2113669922bf9ef7b75db9a8
TinyMurky <murky0830@gmail.com>
2023-06-05 17:35:56 +0800

    [Feat]passport-local strategy register and login complete

--- app.js
+++ app.js
@@ -1,52 +1,60 @@
-//express setup
-const express = require("express")
+// express setup
+const express = require('express')
 const app = express()
 
-//handlebars setup
-const exphbs = require("express-handlebars")
+// handlebars setup
+const exphbs = require('express-handlebars')
 
-//method override for put and delete routing
-const methodOverride = require("method-override")
+// method override for put and delete routing
+const methodOverride = require('method-override')
 
-//express-session for login
-const session = require("express-session")
+// express-session for login
+const session = require('express-session')
 
-//import index.js from ./routes for seperating routing
-const routes = require("./routes")
+// import index.js from ./routes for seperating routing
+const routes = require('./routes')
 
-//passport setting
-const usePassport = require("./configs/passport")
-//import mongoose.js file to connect to MongoDB
-require("./configs/mongoose")
-const Restaurant = require("./models/restaurant")
+// passport setting
+const usePassport = require('./configs/passport')
 
-//import dotenv for .env
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: ".env" })
+// flash store message
+const flash = require('connect-flash')
+// import mongoose.js file to connect to MongoDB
+require('./configs/mongoose')
+
+// import dotenv for .env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ path: '.env' })
 }
-//using port giving by enviroment of cloud server, or default 3000
+// using port giving by enviroment of cloud server, or default 3000
 const PORT = process.env.PORT
 
-//handlebars setting
+// handlebars setting
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs.engine({
-    defaultLayout: "main",
-    helpers: require("./plugins/handlebars-helpers"),
+    defaultLayout: 'main',
+    helpers: require('./plugins/handlebars-helpers')
   })
 )
-app.set("view engine", "handlebars")
+app.set('view engine', 'handlebars')
 
-//preprocessing request
-app.use(express.static("public"))
+// preprocessing request
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
+app.use(methodOverride('_method'))
 app.use(session({
-  secret:process.env.SECRET_SESSION_WORD,
-  resave:false,
+  secret: process.env.SECRET_SESSION_WORD,
+  resave: false,
   saveUninitialized: true
 }))
-app.use(usePassport)
+app.use(flash())
+usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes) // Enable routing to different file.js
 
 app.listen(PORT, () => {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,21 +1,63 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
-const passport = require("passport")
-const User = reqire("../../models/user")
+const passport = require('passport')
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
+const stylesheet = {
+  login: 'login.css',
+  register: 'login.css'
+}
 
-router.get("/login", (req, res)=>{
-  res.render("login", {stylesheet:"login.css"})
+router.get('/login', (req, res) => {
+  res.render('login', { stylesheet: stylesheet.login })
 })
-router.post("/login",
-  passport.authenticate("local", {
-    failureRedirect: "/users/login",
-    failureMessage: true,
+router.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/users/login',
+    failureMessage: true
   }),
   (req, res) => {
-    res.redirect("/")
+    res.redirect('/')
   }
 )
-router.get("/register", (req, res)=>{
-  res.render("register", {stylesheet:"login.css"})
+router.get('/register', (req, res) => {
+  res.render('register', { stylesheet: stylesheet.register })
+})
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!(name && email && password && confirmPassword)) {
+    errors.push({ message: '請輸入所有欄位' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不一致' })
+  }
+  try {
+    const user = await User.findOne({ email })
+    if (user) {
+      errors.push({ message: '該信箱已存在' })
+    }
+    if (errors.length) {
+      res.render('register', {
+        stylesheet: stylesheet.login,
+        errors
+      })
+      return
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      const newUser = new User({
+        name,
+        email,
+        password: hash
+      })
+      newUser.save()
+      req.flash('register_success', '已成功註冊帳號')
+      res.redirect('/users/login')
+    }
+  } catch (error) {
+    console.error(error)
+  }
 })
 module.exports = router
